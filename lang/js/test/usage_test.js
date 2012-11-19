b591fb2aa12c50d4363229e77da1612e418e8146
Quinn Slack <qslack@qslack.com>
2012-11-19 17:53:46 +0800

    test avro field validations

--- lang/js/test/usage_test.js
+++ lang/js/test/usage_test.js
@@ -39,6 +39,16 @@ exports.test = {
     setUp: function(done) {
       this.emptyRecord = {type: 'record', name: 'A', fields: []};
       this.stringFieldRecord = {type: 'record', name: 'StringFieldRecord', fields: [{name: 'stringField', type: 'string'}]};
+      this.manyFieldsRecord = {type: 'record', name: 'ManyFieldsRecord', fields: [
+        {name: 'nullField', type: 'null'},
+        {name: 'booleanField', type: 'boolean'},
+        {name: 'intField', type: 'int'},
+        {name: 'longField', type: 'long'},
+        {name: 'floatField', type: 'float'},
+        {name: 'doubleField', type: 'double'},
+        {name: 'stringField', type: 'string'},
+        {name: 'bytesField', type: 'bytes'}
+      ]};
       done();
     },
     'constructor': function(test) {
@@ -75,22 +85,56 @@ exports.test = {
       test.equal(JSON.stringify(sfr), '{"stringField":"b"}');
       test.done();
     },
-    'Avro field validation': function(test) {
-      var SFR = compileAndEval(this.stringFieldRecord),
+    'Avro field validation': {
+      'StringFieldRecord (simple)': function(test) {
+        var SFR = compileAndEval(this.stringFieldRecord),
         sfr = new SFR();
-      function expectThrows() {
-        test.throws(function() { sfr.stringField = undefined; });
-        test.throws(function() { sfr.stringField = null; });
-        test.throws(function() { sfr.stringField = 5; });
-        test.throws(function() { sfr.stringField = {a: 1}; });
-        test.throws(function() { sfr.stringField = [1,2]; });
-        test.throws(function() { sfr.stringField = function() {}; });
+        function expectThrows() {
+          test.throws(function() { sfr.stringField = undefined; });
+          test.throws(function() { sfr.stringField = null; });
+          test.throws(function() { sfr.stringField = 5; });
+          test.throws(function() { sfr.stringField = {a: 1}; });
+          test.throws(function() { sfr.stringField = [1,2]; });
+          test.throws(function() { sfr.stringField = function() {}; });
+        }
+        expectThrows();
+        sfr.stringField = 'a';
+        expectThrows();
+        test.equal(sfr.stringField, 'a');
+        test.done();
+      },
+      'ManyFieldsRecord': function(test) {
+        var MFR = compileAndEval(this.manyFieldsRecord),
+          mfr = new MFR();
+        function expectThrows() {
+          test.throws(function() { mfr.nullField = undefined; });
+          test.throws(function() { mfr.nullField = 1; });
+          test.throws(function() { mfr.booleanField = 'a'; });
+          test.throws(function() { mfr.intField = 'a'; }); // TODO: warn if setting int/long field to a non-integer
+          test.throws(function() { mfr.longField = 'a'; });
+          test.throws(function() { mfr.floatField = 'a'; });
+          test.throws(function() { mfr.doubleField = 'a'; });
+          test.throws(function() { mfr.stringField = 3; });
+        }
+        expectThrows();
+        mfr.nullField = null;
+        mfr.booleanField = true;
+        mfr.intField = 1;
+        mfr.longField = 2;
+        mfr.floatField = 3.5;
+        mfr.doubleField = 4.5;
+        mfr.stringField = 'a';
+        test.throws(function() { mfr.bytesField = 'a'; }); // TODO: not yet implemented
+        expectThrows();
+        test.equal(mfr.nullField, null);
+        test.equal(mfr.booleanField, true);
+        test.equal(mfr.intField, 1);
+        test.equal(mfr.longField, 2);
+        test.equal(mfr.floatField, 3.5);
+        test.equal(mfr.doubleField, 4.5);
+        test.equal(mfr.stringField, 'a');
+        test.done();
       }
-      expectThrows();
-      sfr.stringField = 'a';
-      expectThrows();
-      test.equal(sfr.stringField, 'a');
-      test.done();
     }
   }
 };
